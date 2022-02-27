3042f0558fc0fc9f66b036b4c8e0eb313baa366c
aaron10l <aaronalvarez7394@gmail.com>
2022-02-26 19:00:11 -0500

    added more stuff

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from level import Level
 
 # Pygame setup
 pygame.init()
-game_active = True
+game_active = False
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 level = Level(level_map,screen)
