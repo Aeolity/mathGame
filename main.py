ff3d394cd75f0aecdad08d2273cf625026797f91
aaron10l <aaronalvarez7394@gmail.com>
2022-02-26 19:22:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ game_active = False
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 level = Level(level_map,screen)
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+test_font = pygame.font.Font('font/Pixeltype.ttf', 80)
 DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 LvL1background = pygame.image.load('graphics/backgrounds/2638149.jpg').convert()
 #Lvl1Background_scaled = pygame.transform.scale(LvL1background, (1725, 1024))
@@ -17,6 +17,9 @@ LvL1background = pygame.image.load('graphics/backgrounds/2638149.jpg').convert()
 #intro screen
 game_name = test_font.render('Mathgame', False, (111,196,169))
 game_name_rect = game_name.get_rect(center = (400,80))
+
+intro_directions = test_font.render('press space to begin', False, (111,196,169))
+intro_directions_rect = intro_directions.get_rect(center = (600, 400))
 
 while True:
 	for event in pygame.event.get():
@@ -26,6 +29,7 @@ while True:
 	if game_active == False:
 		screen.fill((54,38,148))
 		screen.blit(game_name, game_name_rect)
+		screen.blit(intro_directions, intro_directions_rect)
 
 	if game_active:
 		screen.blit(LvL1background,(0,0))
